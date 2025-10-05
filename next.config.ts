b6f1467564b56794474cccee8854efd28563591e
andrea principe ayala <andreaprincipeayala2002@gmail.com>
2025-10-05 15:49:51 +0000

    como puedo agregar esta imagen a "Spaceflight Alters Gut Microbiota-Host

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'images.pexels.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
